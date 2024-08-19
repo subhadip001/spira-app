93ebe080ccc48b8bc74a1430e1e3a2bca2e2a24e
subhadip001 <ssddmm2001@gmail.com>
2024-08-20 02:27:57 +0530

    chore: Update layout to add full-screen video background and hide sidebar

--- app/components/generate-box.tsx
+++ app/components/generate-box.tsx
@@ -32,7 +32,7 @@ const GenerateBox = () => {
 
   return (
     <form
-      className="flex flex-col gap-3 border rounded-lg p-3"
+      className="flex flex-col gap-3 border rounded-lg p-3 bg-white"
       onSubmit={handleSubmit}
     >
       <input
