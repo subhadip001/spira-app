a7295666e0fd6fbe2a9f2b02604e1d604efa5c54
subhadip001 <ssddmm2001@gmail.com>
2024-08-22 08:43:59 +0530

    refactor: Update main section background color and styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import PromptBox from "./_components/prompt-box";
 export default function Home() {
   return (
     <main className="flex min-h-screen">
-      <section className="flex w-full md:m-5 bg-transparent rounded-lg">
+      <section className="flex w-full m-3 md:m-5 bg-white rounded-lg">
         <div className="m-auto w-[60%] px-3 py-2 flex gap-5 flex-col md:h-[50vh]">
           <div className="flex justify-center">
             <Badge>Prototype</Badge>
