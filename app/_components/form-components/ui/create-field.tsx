a5e6c987df318d93c5a1035bbf82b7d31033609a
subhadip001 <ssddmm2001@gmail.com>
2024-10-04 03:07:53 +0530

    Refactor profile edit dialog descriptions

--- app/_components/form-components/ui/create-field.tsx
+++ app/_components/form-components/ui/create-field.tsx
@@ -25,7 +25,7 @@ export function CreateNewField() {
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
