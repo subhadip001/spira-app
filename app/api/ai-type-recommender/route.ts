4c50a8f8dca3a3594288d6decfd3afc80238936b
Thejasvi <thejasvies2001@gmail.com>
2024-10-09 23:06:57 +0530

    add ai suggestion on typing

---
+++ app/api/ai-type-recommender/route.ts
@@ -0,0 +1,29 @@
+import aiApiHandler from "@/lib/ai-api-handler";
+import { AI_POWERED_FORM_TYPE_RECOMMENDER_PROMPT } from "@/lib/utils";
+
+export async function POST(req: Request) {
+  const { prompt } = await req.json();
+
+  if (prompt === undefined || prompt === "") {
+    console.log("Invalid request");
+    return new Response(JSON.stringify({ message: "Invalid request" }), {
+      status: 400,
+    });
+  }
+
+  try {
+    const response = await aiApiHandler("groq", {
+      system_prompt: AI_POWERED_FORM_TYPE_RECOMMENDER_PROMPT,
+      user_question: prompt,
+    });
+
+    return Response.json({ message: response }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { message: "Error processing request" },
+      { status: 500 }
+    );
+  }
+}
+
