b4e6f141d18272ba292106fe2b47629b3471d5be
subhadip001 <ssddmm2001@gmail.com>
2024-11-26 01:06:23 +0530

    go to dashboard button

---
+++ app/_components/custom-button.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React from "react"
+import useAppStore from "@/store/appStore"
+import { useRouter } from "next/navigation"
+import { cn } from "@/lib/utils"
+
+const CustomButton = ({
+  children,
+  handleClick,
+  className,
+}: {
+  children: React.ReactNode | string
+  handleClick: () => void
+  className?: string
+}) => {
+  const user = useAppStore((state) => state.user)
+  const router = useRouter()
+
+  if (!user) {
+    return null
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className={cn(
+        "bg-blue-100 text-spirablue border border-blue-200 hover:bg-blue-200 px-3 py-2 rounded-lg flex items-center",
+        className
+      )}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default CustomButton
