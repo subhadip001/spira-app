4ad2514897fd6e03b885373a98ce57b0667369d9
subhadip001 <ssddmm2001@gmail.com>
2025-01-08 22:12:07 +0530

    protected form and toolbar positioning

--- app/(app)/form/[formId]/layout.tsx
+++ app/(app)/form/[formId]/layout.tsx
@@ -1,6 +1,9 @@
 import EditFormHeader from "@/app/_components/edit-form-header"
+import NotFoundPage from "@/app/_components/not-found-page"
 import SelectVersion from "@/app/_components/select-version"
 import { TooltipProvider } from "@/components/ui/tooltip"
+import { fetchBaseForm } from "@/lib/queries"
+import { createClient } from "@/utils/supabase/server"
 import React from "react"
 
 export default async function Layout(props: {
@@ -10,6 +13,20 @@ export default async function Layout(props: {
   const params = await props.params
 
   const { children } = props
+
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+
+  const response = await fetchBaseForm(params.formId)
+
+  if (response.user_id && response.user_id !== data?.user?.id) {
+    return (
+      <div className="bg-white w-[100vw] h-[100svh]">
+        <NotFoundPage />
+      </div>
+    )
+  }
 
   return (
     <TooltipProvider>
