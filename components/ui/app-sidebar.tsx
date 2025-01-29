f1707e30efad9fc9a1d500d751d38586abaeee7e
subhadip001 <ssddmm2001@gmail.com>
2025-01-30 03:36:56 +0530

    ui changes

--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -34,6 +34,7 @@ import {
 } from "./dropdown-menu"
 import { Icons } from "@/app/_components/icons"
 import { cn } from "@/lib/utils"
+import useAppStore from "@/store/appStore"
 
 const items = [
   {
@@ -61,6 +62,7 @@ const items = [
 export function AppSidebar() {
   const router = useRouter()
   const pathname = usePathname()
+  const user = useAppStore((state) => state.user)
   return (
     <Sidebar>
       <SidebarContent>
@@ -80,10 +82,12 @@ export function AppSidebar() {
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
                     <div
+                      data-active={pathname === item.url}
                       className={cn(
-                        "cursor-pointer",
-                        pathname === item.url &&
-                          "bg-blue-100 text-blue-600 hover:bg-blue-100 hover:text-blue-600"
+                        "flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left h-8 text-sm cursor-pointer transition-colors",
+                        "data-[active=true]:bg-blue-100 data-[active=true]:text-blue-600 data-[active=true]:hover:bg-blue-100 data-[active=true]:hover:text-blue-600",
+                        "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground",
+                        "focus-visible:ring-2 focus-visible:ring-sidebar-ring outline-none"
                       )}
                       onClick={() => {
                         router.push(`${item.url}`)
@@ -105,7 +109,8 @@ export function AppSidebar() {
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton>
-                  <User2 /> Username
+                  <User2 />
+                  <span>{user?.name}</span>
                   <ChevronUp className="ml-auto" />
                 </SidebarMenuButton>
               </DropdownMenuTrigger>
